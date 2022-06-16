ffc7f2569d7944d54980256d805f470da19716d1
lukebella <luca.bellani@studenti.unimi.it>
2022-06-16 15:15:14 +0200

    Returning MIDI file path HTTP response

--- server_model.py
+++ server_model.py
@@ -1,25 +1,25 @@
 from load_and_sample import Model
 import cherrypy
+import requests
 
 class ModelServer():
 
     def __init__(self):
         self.model = Model()
+        self.url = 'http://127.0.0.1:8080'
 
     @cherrypy.expose
     def load(self, ckpt_path='C:/Users/lenovo/Tesi/prova/Transformer/unconditional_model_16.ckpt'):
-        print(ckpt_path)
         self.model.load(ckpt_path)
 
 
     @cherrypy.expose
     def sample(self):
-        self.model.sample()
+        return self.model.sample()
+        #f = requests.get(url)
+        # print(f.text)
         #rispondere in http con un file
 
 if __name__ == '__main__':
     cherrypy.quickstart(ModelServer())
-    """m = ModelServer()
 
-    m.load()
-    m.sample()"""
