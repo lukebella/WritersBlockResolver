2d17d52d4e45ee61f229e6082e116dff16bfbcf0
lukebella <luca.bellani@studenti.unimi.it>
2022-06-27 14:50:49 +0200

    Fixing code.

--- server_model.py
+++ server_model.py
@@ -1,26 +1,22 @@
 from load_and_sample import Model
 import cherrypy
 from cherrypy.lib.static import serve_file
-import mimetypes
 
 
 class ModelServer():
 
     def __init__(self):
         self.model = Model()
-        self.filename = ""
 
     @cherrypy.expose
-    def load(self, ckpt_path='./Transformer/unconditional_model_16.ckpt'):
+    def load(self, ckpt_path):   #'./Transformer/unconditional_model_16.ckpt'
         self.model.load(ckpt_path)
         return
 
     @cherrypy.expose
     def sample(self):
-        self.filename = self.model.sample()
-        mimetypes.types_map['.mid'] = 'audio/midi'
-        return serve_file(self.filename, "application/x-download", "attachment")
-
+        filename = self.model.sample()
+        return serve_file(filename, "audio/midi", "attachment", "Unconditional Midi File")
 
 
 if __name__ == '__main__':
